45b3c37dfb8b0440764c8f76c79a109f1dbd1ec0
Luminite2 <prkriley@gmail.com>
2017-11-09 15:34:49 -0500

    More skeleton

--- src/revlog.py
+++ src/revlog.py
@@ -1,11 +1,19 @@
-#============================================================================
 class Revlog:
-  def new(self):
-    pass
-  
-  def content(revision_str):
-    pass
-  
-  def commit(new_revision_str):
-    pass
+    filename = None
+    revlogLoc = None
 
+    def __init__(fname, revloc=None):
+        if revloc == None:
+            revloc = ".hg/revlogs/" + filename + ".rl"
+
+        filename = fname
+        revlogLoc = revloc
+
+    def new():
+        pass
+
+    def content(revision):
+        pass
+
+    def commit(newrevision):
+        pass
