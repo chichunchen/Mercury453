305169b9a66b89a9dd6f797584e76798332802d8
Taylan K. Sen <tsen@cs.rochester.edu>
2017-11-09 14:49:28 -0500

    added base class for Manifest and Revlog

--- src/interfaces.py
+++ src/interfaces.py
@@ -28,24 +28,24 @@ class Repository:
   def checkout(self, revision_str): 
     """ Description: restores the repository directory to how it was at the 
         given revision
-    Precondition: the revision number is valid
-    Postcondition: the directory contents reflect how they were at the given
-    revision	
-    Main procedure: restore the manifest to the given revision (via 
-    Manifest/Revlog), then use Manifest to restore directory contents
-    Exception: if this revision number is not valid, fail
+        Precondition: the revision number is valid
+        Postcondition: the directory contents reflect how they were at the given
+        revision	
+        Main procedure: restore the manifest to the given revision (via 
+        Manifest/Revlog), then use Manifest to restore directory contents
+        Exception: if this revision number is not valid, fail
     """
     pass
 
   def commit(self):
     """ Description: commit the specified files or all outstanding changes
         Precondition: files are staged for commit and no argument is provided, 
-    or no files are staged and some are provided as arguments
-    Postcondition: the specified files or outstanding changes are committed
-    Main procedure: if there are staged changes, commit them; if files are
-    passed as arguments, stage and commit them
-    Exception: if one or more files are staged and one or more arguments are
-    provided, fail
+        or no files are staged and some are provided as arguments
+        Postcondition: the specified files or outstanding changes are committed
+        Main procedure: if there are staged changes, commit them; if files are
+        passed as arguments, stage and commit them
+        Exception: if one or more files are staged and one or more arguments are
+        provided, fail
     """
     pass
 
@@ -61,34 +61,34 @@ class Repository:
   def delete(self, files_list):
     """ Description: remove the specified files from the next commit
         Precondition: file is staged for commit
-    Postcondition: file is not staged for commit
-    Main procedure: find file in list of staged files and remove it
-    Exception: if the file is not staged, fail
+        Postcondition: file is not staged for commit
+        Main procedure: find file in list of staged files and remove it
+        Exception: if the file is not staged, fail
     """
     pass
 
-  def merge(path_str):
+  def merge(self, path_str):
     """ Description: merge with the repository located at path.
         Precondition: path contains a valid repository
-    Postcondition: contents and history of branch present path exist in this
-    repository
-    Main procedure: merge the two repositories first by merging their DAGs,
-    and then finding the lowest common ancestor for each file; each file is
-    merged (see Analysis document)
-    Exception: if path does not contain a repository, or if the repository 
-    is determined to be identical to the current repository, fail
+        Postcondition: contents and history of branch present path exist in this
+        repository
+        Main procedure: merge the two repositories first by merging their DAGs,
+        and then finding the lowest common ancestor for each file; each file is
+        merged (see Analysis document)
+        Exception: if path does not contain a repository, or if the repository 
+        is determined to be identical to the current repository, fail
     """
     pass
 
   def status(self):
     """ Description: display files changed but not committed
         Precondition: current directory is a repository
-    Postcondition: directory contents that have been edited are 
-    displayed, grouped by status: staged for commit, or edited but not 
-    staged
-    Main procedure: list all files staged for commit, then find and 
-    display all edited but unstaged files
-    Exception: if current directory is not a repository, fail 
+        Postcondition: directory contents that have been edited are 
+        displayed, grouped by status: staged for commit, or edited but not 
+        staged
+        Main procedure: list all files staged for commit, then find and 
+        display all edited but unstaged files
+        Exception: if current directory is not a repository, fail 
     """
     pass
 
@@ -96,21 +96,38 @@ class Repository:
   def history(self):
     """ Description: display commit history
         Precondition: current directory is a repository
-    Postcondition: display the history of all commits
-    Main procedure: display a list of each commit, its parent(s), revision
-    number, and commit message
-    Exception: if current directory is not a repository, fail
+        Postcondition: display the history of all commits
+        Main procedure: display a list of each commit, its parent(s), revision
+        number, and commit message
+        Exception: if current directory is not a repository, fail
     """
     pass	
 
 #============================================================================
 class Manifest:
-  pass
+  def new(self):
+    pass
+  
+  def commit(self, files_list, new_revision_str):
+    pass
+  
+  def checkout(self, revision_str):
+    pass
+  
+  def merge(revision_str):
+    pass
 
   
 #============================================================================
 class Revlog:
-  pass
+  def new(self):
+    pass
+  
+  def content(revision_str):
+    pass
+  
+  def commit(new_revision_str):
+    pass
 
 #=============================================================================
 if __name__ == '__main__':
--- src/repository.py
+++ src/repository.py
@@ -32,24 +32,24 @@ class RepositoryImpl(interfaces.Repository):
   def checkout(self, revision_str): 
     """ Description: restores the repository directory to how it was at the 
         given revision
-	Precondition: the revision number is valid
-	Postcondition: the directory contents reflect how they were at the given
-	revision	
-	Main procedure: restore the manifest to the given revision (via 
-	Manifest/Revlog), then use Manifest to restore directory contents
-	Exception: if this revision number is not valid, fail
+        Precondition: the revision number is valid
+        Postcondition: the directory contents reflect how they were at the given
+        revision	
+        Main procedure: restore the manifest to the given revision (via 
+        Manifest/Revlog), then use Manifest to restore directory contents
+        Exception: if this revision number is not valid, fail
     """
     pass
 
   def commit(self):
     """ Description: commit the specified files or all outstanding changes
         Precondition: files are staged for commit and no argument is provided, 
-	or no files are staged and some are provided as arguments
-	Postcondition: the specified files or outstanding changes are committed
-	Main procedure: if there are staged changes, commit them; if files are
-	passed as arguments, stage and commit them
-	Exception: if one or more files are staged and one or more arguments are
-	provided, fail
+        or no files are staged and some are provided as arguments
+        Postcondition: the specified files or outstanding changes are committed
+        Main procedure: if there are staged changes, commit them; if files are
+        passed as arguments, stage and commit them
+        Exception: if one or more files are staged and one or more arguments are
+        provided, fail
     """
     pass
 
@@ -65,32 +65,32 @@ class RepositoryImpl(interfaces.Repository):
   def delete(self, files_list):
     """ Description: remove the specified files from the next commit
         Precondition: file is staged for commit
-	Postcondition: file is not staged for commit
-	Main procedure: find file in list of staged files and remove it
-	Exception: if the file is not staged, fail
+        Postcondition: file is not staged for commit
+        Main procedure: find file in list of staged files and remove it
+        Exception: if the file is not staged, fail
     """
     pass
 
-  def merge(path_str):
+  def merge(self, path_str):
     """ Description: merge with the repository located at path.
         Precondition: path contains a valid repository
-	Postcondition: contents and history of branch present path exist in this
-	repository
-	Main procedure: merge the two repositories first by merging their DAGs,
-	and then finding the lowest common ancestor for each file; each file is
-	merged (see Analysis document)
-	Exception: if path does not contain a repository, or if the repository 
-	is determined to be identical to the current repository, fail
+        Postcondition: contents and history of branch present path exist in this
+        repository
+        Main procedure: merge the two repositories first by merging their DAGs,
+        and then finding the lowest common ancestor for each file; each file is
+        merged (see Analysis document)
+        Exception: if path does not contain a repository, or if the repository 
+        is determined to be identical to the current repository, fail
     """
     pass
 
   def status(self):
     """ Description: display files changed but not committed
         Precondition: current directory is a repository
-	Postcondition: directory contents that have been edited are 
-	displayed, grouped by status: staged for commit, or edited but not 
-	staged
-	Main procedure: list all files staged for commit, then find and 
+        Postcondition: directory contents that have been edited are 
+        displayed, grouped by status: staged for commit, or edited but not 
+        staged
+        Main procedure: list all files staged for commit, then find and 
 		  display all edited but unstaged files
 		  Exception: if current directory is not a repository, fail 
     """
@@ -100,10 +100,10 @@ class RepositoryImpl(interfaces.Repository):
   def history(self):
     """ Description: display commit history
         Precondition: current directory is a repository
-	Postcondition: display the history of all commits
-	Main procedure: display a list of each commit, its parent(s), revision
-	number, and commit message
-	Exception: if current directory is not a repository, fail
+        Postcondition: display the history of all commits
+        Main procedure: display a list of each commit, its parent(s), revision
+        number, and commit message
+        Exception: if current directory is not a repository, fail
     """
     pass	
  
