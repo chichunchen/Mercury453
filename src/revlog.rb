486d32e1fc699fd9530389596d136d771debb72a
chichunchen <chichunchen844@gmail.com>
2017-11-17 16:05:38 -0500

    Implement basic checkout and content in revlog

--- src/revlog.rb
+++ src/revlog.rb
@@ -28,11 +28,28 @@ class Revlog
 
     # return the content of a given revision
     def content(revision)
+        line = revision+1
+        parse = parse_indexfile_line get_indexfile_line(line)
+        offset = parse[1]
+        length = parse[2]
+        str = ""
+
+        # read file from offset to (offset+length-1)
+        (offset...(offset+length)).each do |l|
+            str += IO.readlines(@datafile)[l]
+        end
+
+        str
     end
 
     # copy the revision to current working directory
     def checkout(revision)
-      
+        str = content revision
+
+        # write str to current directory
+        File.open(@fname, "w") do |f|
+            f.write str
+        end
     end
     
     # add file content as a new revision
@@ -47,7 +64,7 @@ class Revlog
 
         # new version is the old version plus 1
         # new offset is the last length + last offset
-        parse_last_line = IO.readlines(@indexfile)[-1].scanf("%d %d %d")
+        parse_last_line = parse_indexfile_line get_indexfile_line(-1)
         new_version = parse_last_line[0] + 1
         new_offset = parse_last_line[1] + parse_last_line[2]
 
@@ -68,9 +85,19 @@ class Revlog
         def line_count_to_s filename
             IO.readlines(filename).size.to_s
         end
+
+        # return a line with given line number
+        def get_indexfile_line number
+            IO.readlines(@indexfile)[number]
+        end
+
+        def parse_indexfile_line line
+            line.scanf("%d %d %d")
+        end
 end
 
 ## simple unit tests
 r = Revlog.new "aaa.txt", ""
 r.commit 1
 r.commit 2
+r.checkout 1
