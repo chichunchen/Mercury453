65401d6d4f162011cf655477570256755e301ba7
Luminite2 <prkriley@gmail.com>
2017-11-18 18:33:33 -0500

    Dag Backing

--- src/mergemodules/repomerge.rb
+++ src/mergemodules/repomerge.rb
@@ -1,29 +1,66 @@
-require 'yaml'
+require 'fileutils'
+
+class BackedHash < Hash
+    def initialize(f)
+        @f = f
+    end
+
+    def init(h)
+        clear
+        h.each do |e|
+            self[e[0]] = e[1]
+        end
+        save
+        self
+    end
+
+    def []=(key,val)
+        ret = super(key,val)
+        save
+        ret
+    end
+
+    def save
+        File.open(@f, "w") do |f|
+            f.write(Marshal::dump(self))
+        end
+    end
+end
 
 module RepoMerge
     DAG_LOC = ".repository/revisions.yaml"
+    DEFAULT_DAG = {1 => []}
 
-    #def foo
-    #    5
-    #end
+    def dag
+        return @dag if @dag
+        begin
+            File.open(DAG_LOC, "r") do |f|
+                @dag = Marshal::load(f)
+            end
+        rescue Errno::ENOENT
+            d = File.dirname(DAG_LOC)
+            unless File.directory?(d)
+                FileUtils.mkdir_p(d)
+            end
+            @dag = BackedHash.new(DAG_LOC)
+            @dag.init(DEFAULT_DAG)
+            @dag
+        end
+    end
 
-   # def bar
-   #     @wat = 4
-   # end
+    def dag=(val)
+        d = dag
+        d.init(val)
+        @dag = d
+    end
 
-   #store/recover dag
-    #
-
-    
 end
 
 module Repo
     extend RepoMerge
 
-    def Repo.wat
-        foo
-    end
-
 end
 
-p Repo.wat
+#p Repo.dag
+#Repo.dag[2] = []
+#p Repo.dag
