23ec076dc98a604446a9c4a422ae81a6d9b79ee2
Luminite2 <prkriley@gmail.com>
2017-11-17 21:05:55 -0500

    Manifest changes

--- src/manifest.rb
+++ src/manifest.rb
@@ -1,5 +1,10 @@
 require_relative 'revlog'
 #REVLOG_LOC = ".hg/manifest/rl"
+HIDDEN_DIR = ".hg"
+MANIFEST_NAME = "manifest"
+MANIFEST_FULL_PATH = HIDDEN_DIR + "/manifest/" + MANIFEST_NAME
+MANIFEST_INDEX = HIDDEN_DIR + "/manifest/index/" + MANIFEST_NAME
+MANIFEST_DATA = HIDDEN_DIR + "/manifest/data/" + MANIFEST_NAME
 
 class Manifest
     #@manlog = nil #the revlog representing this manifest file
@@ -7,7 +12,7 @@ class Manifest
 
     def initialize(rootPath, revision)
         #@manlog = Revlog.new(rootPath+REVLOG_LOC) #revlog representing this manifest file
-        @manlog = Revlog.new(,nil)
+        @manlog = Revlog.new(MANIFEST_NAME, MANIFEST_DATA, MANIFEST_INDEX)
         @revnum = revision
     end
 
